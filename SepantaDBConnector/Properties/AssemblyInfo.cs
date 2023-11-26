4645dc9eca0bc75a9f4fa37dd37a02dc6f44e3c9
Houman Farokhi <farokhihouman@gmail.com>
2023-11-26 15:49:08 +0330

    Make Assembly

--- SepantaDBConnector/Properties/AssemblyInfo.cs
+++ SepantaDBConnector/Properties/AssemblyInfo.cs
@@ -1,25 +1,30 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+
+// ReSharper disable ArgumentsStyleStringLiteral
+// ReSharper disable ArgumentsStyleLiteral
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle(title: "SepantaDBConnector")]
-[assembly: AssemblyDescription(description: "")]
-[assembly: AssemblyConfiguration(configuration: "")]
-[assembly: AssemblyCompany(company: "")]
-[assembly: AssemblyProduct(product: "SepantaDBConnector")]
-[assembly: AssemblyCopyright(copyright: "Copyright ©  2022")]
-[assembly: AssemblyTrademark(trademark: "")]
-[assembly: AssemblyCulture(culture: "")]
+[assembly: AssemblyTitle("SepantaDBConnector")]
+[assembly: AssemblyDescription("Connect your Program to Its DB using registry")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Houman Farokhi")]
+[assembly: AssemblyProduct("SepantaDBConnector")]
+[assembly: AssemblyCopyright("Copyright HoumanFarokhi (farokhihouman@gmail.com) ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(visibility: false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid(guid: "d3bf9da8-f0a4-4235-a08a-6b125d80eb4c")]
+[assembly: Guid("d3bf9da8-f0a4-4235-a08a-6b125d80eb4c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(version: "1.0.0.0")]
-[assembly: AssemblyFileVersion(version: "1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("fa-IR")]
